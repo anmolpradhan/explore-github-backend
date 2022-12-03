36373032b85d48e77c45904f2591e68e5894bb3d
anmolpradhan <anamolpradhan@gmail.com>
2022-12-03 11:07:46 +0545

    Added middleware to server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-
+import { setHeaders } from './middleware';
 const app=express();
 const PORT=8000;
 
 app.use(express.json());
 app.use(cors());
-
+app.use(setHeaders())
 app.get('/',(req,res)=>{
     res.send("Welcome")
 })
